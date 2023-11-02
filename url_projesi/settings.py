9c0519d785e017cc853ce74cd34392b39acf4b98
Kubilay Birer <kubilaybirer@hotmail.com>
2023-11-02 14:31:13 +0300

    succesfuly docked the project to alpinelinux

--- url_projesi/settings.py
+++ url_projesi/settings.py
@@ -11,21 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u=iut=8-8k7$v-xqwii%r@4@boay!__g5(vt)0p$kvygo52xa#'
+DEBUG = os.environ.get("DEBUG")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
